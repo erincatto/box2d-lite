3e3dccb06d1e92ae40ed2e885125430122999769
Erin Catto <erincatto@users.noreply.github.com>
2019-01-20 20:29:10 -0800

    Issue 1

--- src/Collide.cpp
+++ src/Collide.cpp
@@ -167,9 +167,6 @@ int Collide(Contact* contacts, Body* bodyA, Body* bodyB)
 	Mat22 RotAT = RotA.Transpose();
 	Mat22 RotBT = RotB.Transpose();
 
-	Vec2 a1 = RotA.col1, a2 = RotA.col2;
-	Vec2 b1 = RotB.col1, b2 = RotB.col2;
-
 	Vec2 dp = posB - posA;
 	Vec2 dA = RotAT * dp;
 	Vec2 dB = RotBT * dp;
